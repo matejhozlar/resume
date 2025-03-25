da351f243a4cf3e96a070e2364a90c4a00177b14
Matej <hozlarmatej0@gmail.com>
2025-03-25 23:22:43 +0100

    DB connection and url fixes

--- gameverse/server/server.js
+++ gameverse/server/server.js
@@ -1,7 +1,34 @@
 import express from "express";
+import env from "dotenv";
+import bodyParser from "body-parser";
+import pg from "pg";
+import flash from "connect-flash";
+import session from "express-session";
+import { Strategy } from "passport-local";
+import passport from "passport";
+import bcrypt from "bcrypt";
 
+env.config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+db.connect();
+
+app.get("/login", (req, res) => {
+  const message = req.flash("error") || [];
+  res.render("login.ejs", { message: message });
+});
 
 app.get("/api", (req, res) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
