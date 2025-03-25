bbaf2c4e2fa8d19f68615ece180f7ab74455367b
Matej <hozlarmatej0@gmail.com>
2025-03-25 17:24:10 +0100

    Translation, animation fixes

--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,24 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
     const allElements = document.querySelectorAll("section, h2, img, p, ul");
     const sections = Array.from(allElements).filter(el => !el.closest(".no-fade"));
-
+  
     const options = {
-        root: null, 
-        threshold: 0.1, 
-        rootMargin: "0px"
+      root: null,
+      threshold: 0.1,
+      rootMargin: "0px"
     };
-
+  
     const fadeInOnScroll = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("fade-in-visible");
-                observer.unobserve(entry.target);
-            }
-        });
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fade-in-visible");
+          observer.unobserve(entry.target);
+        }
+      });
     }, options);
-
+  
     sections.forEach(section => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top > window.innerHeight || rect.bottom < 0) {
         section.classList.add("fade-in-hidden");
         fadeInOnScroll.observe(section);
+      } else {
+        section.classList.add("fade-in-visible");
+      }
     });
-});
+  });
