f048765448042e75138b7436671996f47fbe5582
Matej <hozlarmatej0@gmail.com>
2025-03-25 23:07:00 +0100

    Creating api connection

---
+++ gameverse/server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
