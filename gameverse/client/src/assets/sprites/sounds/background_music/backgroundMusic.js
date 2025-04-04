d0631389787a729d0d7841782b7a3dd83f20dd77
Matej <hozlarmatej0@gmail.com>
2025-04-04 20:09:22 +0200

    Adding background music

---
+++ gameverse/client/src/assets/sprites/sounds/background_music/backgroundMusic.js
@@ -0,0 +1,8 @@
+const backgroundMusic = {
+  music1: require("./background1.mp3"),
+  music2: require("./background2.mp3"),
+  music3: require("./background3.mp3"),
+  music4: require("./background3.mp3"),
+};
+
+export default backgroundMusic;
