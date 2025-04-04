329deea22e42fae5caae55d2cd39fc8894da35da
Matej <hozlarmatej0@gmail.com>
2025-04-04 17:15:48 +0200

    Adding scripts for sounds

---
+++ gameverse/client/src/assets/sprites/sounds/shootSoundHandgun.js
@@ -0,0 +1,7 @@
+const shootSoundHandgun = {
+  shoot1: require("./handgun1.mp3"),
+  shoot2: require("./handgun2.mp3"),
+  shoot3: require("./handgun3.mp3"),
+};
+
+export default shootSoundHandgun;
