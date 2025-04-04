2226060e6f1dda6810ab053186b78e7fba0f441f
Matej <hozlarmatej0@gmail.com>
2025-04-04 12:04:44 +0200

    Configuring rifle shooting sounds

--- gameverse/client/src/assets/sprites/sounds/shootSoundRifle.js
+++ gameverse/client/src/assets/sprites/sounds/shootSoundRifle.js
@@ -1,6 +1,6 @@
 const shootSoundRifle = {
-  shoot1: require("./single2.mp3"),
-  shoot2: require("./single1.mp3"),
+  shoot1: require("./single1.mp3"),
+  shoot2: require("./single2.mp3"),
 };
 
 export default shootSoundRifle;
