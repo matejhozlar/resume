1ee494d4733e299b82d0c345179d43c76a4830b3
Matej <hozlarmatej0@gmail.com>
2025-04-04 11:33:12 +0200

    Adding shooting animations

---
+++ gameverse/client/src/assets/sprites/shooting/index.js
@@ -0,0 +1,7 @@
+const shootRifle = {
+  shoot1: require("./survivor-shoot_rifle_0.png"),
+  shoot2: require("./survivor-shoot_rifle_1.png"),
+  shoot3: require("./survivor-shoot_rifle_2.png"),
+};
+
+export default shootRifle;
